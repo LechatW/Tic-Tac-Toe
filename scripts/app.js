5966f2ad1cf2064028542310916c21afba6d5b0e
LechatW <williamlechat@hotmail.com>
2021-12-02 13:30:28 +0100

    added input validation and errors style

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,17 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayerOneBtnElement = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtnElement = document.getElementById('edit-player-2-btn');
+
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const confirmConfigBtnElement = document.getElementById('confirm-config-btn');
 
 editPlayerOneBtnElement.addEventListener('click', openPlayerConfig);
 editPlayerTwoBtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+formElement.addEventListener('submit', savePlayerConfig);
 
